781bc53874388c9305e07b0fb2a28384432a4767
ioncrash <ioncrash@gmail.com>
2017-01-13 15:05:53 -0500

    changeVideo succesfully finds =
    now splice string

--- app/components/youtube-finder/component.js
+++ app/components/youtube-finder/component.js
@@ -3,7 +3,9 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   actions: {
     changeVideo() {
-      console.log('in changeVideo, url: ', this.get('url'));
+      let url = this.get('url');
+      let equalIndex = url.indexOf('=');
+      console.log("the equal is at ", equalIndex);
     }
   }
 });
