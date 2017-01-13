006b5da2abfc9cf4b490e784cd860b27b4d21aff
ioncrash <ioncrash@gmail.com>
2017-01-13 15:08:22 -0500

    change video successfully isolates ytid
    now send to ember-youtube

--- app/components/youtube-finder/component.js
+++ app/components/youtube-finder/component.js
@@ -4,8 +4,7 @@ export default Ember.Component.extend({
   actions: {
     changeVideo() {
       let url = this.get('url');
-      let equalIndex = url.indexOf('=');
-      console.log("the equal is at ", equalIndex);
+      console.log(url.split('=')[1]);
     }
   }
 });
