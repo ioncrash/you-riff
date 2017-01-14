93a85636ec7d589a61a05f31381079f8bf44d619
ioncrash <ioncrash@gmail.com>
2017-01-14 15:06:01 -0500

    trying to develop correct behavior for changeVideo
    seems to always transition to videos/1 -- opened an issue... working on
    riff component instead

--- app/index/route.js
+++ app/index/route.js
@@ -2,6 +2,20 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model () {
-    return this.get('store').findRecord('video', 1);
+    return this.get('store').findAll('video');
   },
+  actions: {
+    changeVideo(ytid) {
+      console.log(ytid);
+      this.store.queryRecord( 'video', { ytid: ytid } )
+        .then((video_id) => {
+          this.transitionTo('video', video_id);
+        });
+      // if(result){
+
+      // } else {
+      // console.log('in else');
+      // }
+    }
+  }
 });
