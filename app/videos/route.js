22c4c68d0fc0b42a693f694a4e66169a3991c8dd
ioncrash <ioncrash@gmail.com>
2017-01-14 06:47:28 -0500

    add button to transition to video route
    I think it'll work?

--- app/videos/route.js
+++ app/videos/route.js
@@ -4,4 +4,10 @@ export default Ember.Route.extend({
   model () {
     return this.get('store').findAll('video');
   },
+  actions: {
+    openVideo(video) {
+      this.transitionTo('video', video)
+      // console.log('inside openVideo, video is: ', video);
+    }
+  }
 });
