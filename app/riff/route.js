50ce8a89a836b7daca15fb4d13a8df55aefa8046
ioncrash <ioncrash@gmail.com>
2017-01-15 19:41:43 -0500

    refactor edit riff form into edit-riff-panel
    now attach action handler, send up to riff route

--- app/riff/route.js
+++ app/riff/route.js
@@ -4,4 +4,9 @@ export default Ember.Route.extend({
   model (params) {
     return this.get('store').findRecord('riff', params.riff_id);
   },
+  actions: {
+    save() {
+      this.get('model').save();
+    }
+  }
 });
