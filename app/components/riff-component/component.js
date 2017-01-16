edc5de069eddcbd01e58898ccf9756b1128f39ff
ioncrash <ioncrash@gmail.com>
2017-01-16 00:51:09 -0500

    cleanup extra computed property for stamp

--- app/components/riff-component/component.js
+++ app/components/riff-component/component.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   currentTime: 0,
-  stamp: Ember.computed('riff', function() {return this.get('riff').get('stamp')}),
 
   isDisplayed: Ember.computed('currentTime', 'stamp', function() {
     return this.get('riff').get('stamp') < this.get('currentTime');
