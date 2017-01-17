f1a135aafb103e0942313b19c43003367a3c3f30
ioncrash <ioncrash@gmail.com>
2017-01-17 17:48:22 -0500

    if/then for email equality now works!
    needed to declare full path to attribute in parameters of Ember.computed

--- app/components/edit-riff-panel/component.js
+++ app/components/edit-riff-panel/component.js
@@ -4,8 +4,8 @@ export default Ember.Component.extend({
   auth: Ember.inject.service(),
   user: Ember.computed.alias('auth.credentials.email'),
 
-  isUser: Ember.computed('user', 'riff', function() {
-    return this.get('riff').get('user') === this.get('user');
+  isUser: Ember.computed('user', 'riff.user.email', function() {
+    return this.get('riff.user.email') === this.get('user');
   }),
 
   actions: {
@@ -18,6 +18,10 @@ export default Ember.Component.extend({
     deleteRiff() {
       this.sendAction('deleteRiff', this.get('riff'));
       // console.log(this.get('riff'))
+    },
+    flagRiff() {
+      console.log(this.get('riff.user.email'))
+      console.log(this.get('user'))
     }
   }
 });
