3d9fac97c62e5c0847f09b72ccbba55c0515673b
ioncrash <ioncrash@gmail.com>
2017-01-16 19:26:31 -0500

    starting to build newRiff inside chat-box
    need to figure out how to get user in here...

--- app/components/chat-box/component.js
+++ app/components/chat-box/component.js
@@ -2,4 +2,20 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNameBindings: ['chat-box'],
+
+  newRiff: {
+    text: null,
+    flagged: false,
+    stamp: Ember.computed('currentTime', function() {
+      return this.get('currentTime');
+    }),
+  },
+
+  actions: {
+    createRiff() {
+      let data = this.get('newRiff');
+      data.video = this.get('video');
+      console.log('newRiff = ', this.get('newRiff'));
+    }
+  }
 });
