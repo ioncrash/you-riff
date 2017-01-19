f945301fd167e4211847486dff1bb2e6862571be
ioncrash <ioncrash@gmail.com>
2017-01-19 15:08:06 -0500

    disable edit buttons if not correct user
    or if not signed in

--- app/components/edit-riff-panel/component.js
+++ app/components/edit-riff-panel/component.js
@@ -3,6 +3,10 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   auth: Ember.inject.service(),
   user: Ember.computed.alias('auth.credentials.email'),
+  isAuthenticated: Ember.computed.alias('auth.isAuthenticated'),
+  cantEdit: Ember.computed('isAuthenticated', function() {
+    return !this.get('isAuthenticated') || !this.get('isUser');
+  }),
 
   isUser: Ember.computed('user', 'riff.user.email', function() {
     return this.get('riff.user.email') === this.get('user');
